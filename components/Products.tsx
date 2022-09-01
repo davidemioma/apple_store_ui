394da1d54925f87e4bf8f8b6b912dc787031d5ae
davidemioma <davidemiomauche@gmail.com>
2022-09-01 15:53:20 +0100

    key prop

--- components/Products.tsx
+++ components/Products.tsx
@@ -10,7 +10,7 @@ const Products = ({ products }: Props) => {
   return (
     <div className="mx-auto grid max-w-fit justify-items-center gap-8 px-8 pb-24 pt-10 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
       {products.map((item) => (
-        <Product product={item} />
+        <Product key={item._id} product={item} />
       ))}
     </div>
   );
